7808ce8583e284bac43663a32658616bebd6e8c3
Alvi Choirinnikmah <avialvi08@gmail.com>
2025-03-05 23:31:53 +0700

    Perubahan 9

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,9 +11,7 @@ class UserController extends Controller
     public function index()
     {
         //coba akses model UserModel
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
@@ -63,3 +61,13 @@ class UserController extends Controller
         //     abort(404);
         // });
         // return view('user', ['data' => $user]);
+    
+    //Modifikasi 7
+        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+    //Modifikasi 8
+        // $user = UserModel::findOrFail(1);
+        //     return view('user', ['data' => $user]);
