615a0435566f9a223b847e12400c95b9e57096e4
Alvi Choirinnikmah <avialvi08@gmail.com>
2025-03-12 11:09:20 +0700

    Modifikasi (Praktikum 1)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,16 @@
 @extends('layout.app')
 
 {{-- Customize layout sections --}}
+@section('title', 'Dashboard')
 
-@section('subtitle', 'Welcome')
-@section('content_header_title', 'Home')
-@section('content_header_subtitle', 'Welcome')
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 {{-- Content body: main page content --}}
 
 @section('content_body')
-    <p>Welcome to this beatiful admin panel.</p>
+    <p>Welcome to this beautiful admin panel.</p>
 @stop
 
 {{-- Push extra CSS --}}
